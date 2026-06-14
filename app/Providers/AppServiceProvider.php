0f3efee6f5dd01fd4feb1faa8d3c7176241e663a
hugov-costa <hugodevx@gmail.com>
2026-06-14 18:43:27 -0300

    docs: generate api documentation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+            /** @var SecurityScheme $scheme */
+            $scheme = SecurityScheme::http('bearer', 'sanctum');
+            $openApi->secure($scheme);
+
+            $openApi->info->title = 'Soft-line API';
+            $openApi->info->description = 'Documentação da API de clientes e produtos para o teste técnico da Soft-line.';
+            $openApi->info->version = '0.1';
+        });
+
+        Scramble::routes(function (Route $route) {
+            return Str::startsWith($route->uri, 'api/');
+        });
     }
 }
