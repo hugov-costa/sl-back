677384ddd3ced994a11e1e5e2b3180600729df98
hugov-costa <hugodevx@gmail.com>
2026-06-14 21:33:59 -0300

    chore: add localization to validation messages

---
+++ pt_BR/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma de Paginação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são usadas pela biblioteca de paginação para criar
+    | links de paginação simples. Sinta-se livre para alterá-los para qualquer coisa
+    | que desejar para personalizar suas visualizações e melhor corresponder à sua aplicação.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Próximo &raquo;',
+
+];
---
+++ pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma de Recuperação de Senha
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são as linhas padrão que correspondem aos motivos
+    | fornecidos pelo gerenciador de senha para uma tentativa de atualização de senha
+    | resultado como falha devido a uma senha / token de redefinição inválido.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida.',
+    'sent' => 'Enviamos seu link de redefinição de senha por email.',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não conseguimos encontrar um usuário com esse endereço de email.",
+
+];
