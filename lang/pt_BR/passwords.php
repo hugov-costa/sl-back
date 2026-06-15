62637f9bad820141e051649e42dd759fa3f5f230
hugov-costa <hugodevx@gmail.com>
2026-06-14 21:35:46 -0300

    style: apply pint format

--- lang/pt_BR/passwords.php
+++ lang/pt_BR/passwords.php
@@ -17,6 +17,6 @@ return [
     'sent' => 'Enviamos seu link de redefinição de senha por email.',
     'throttled' => 'Aguarde antes de tentar novamente.',
     'token' => 'Este token de redefinição de senha é inválido.',
-    'user' => "Não conseguimos encontrar um usuário com esse endereço de email.",
+    'user' => 'Não conseguimos encontrar um usuário com esse endereço de email.',
 
 ];
